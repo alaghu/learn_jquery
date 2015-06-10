c570e7a7f4b5ba0d760f16a052e1a827164816aa
Anand Alaghu <anand@mannerweb.com>
2015-06-10 02:08:44 +0000

    initial

---
+++ app/controllers/intros_controller.rb
@@ -0,0 +1,10 @@
+class IntrosController < ApplicationController
+  before_action :set_intro, only: [:show, :edit, :update, :destroy]
+
+
+  def index
+    @intro_anand = 'hello introduction'
+  end
+
+
+end
