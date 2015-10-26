36e60ffc68ba0302c5cdc6c8edad2fe9d16f67a5
Anand Alaghu <anand@loremore.com>
2015-10-26 09:56:57 -0700

    cleaned up intros controller.

--- app/controllers/intros_controller.rb
+++ app/controllers/intros_controller.rb
@@ -1,10 +1,8 @@
+# Inheriting from Application controller.
 class IntrosController < ApplicationController
   before_action :set_intro, only: [:show, :edit, :update, :destroy]
-
 
   def index
     @intro_anand = 'hello introduction'
   end
-
-
 end
