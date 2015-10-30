f3926cd014fdb1b6a69fa5015acc4b12765c5022
Anand Alaghu <anand@loremore.com>
2015-10-29 22:36:19 -0700

    reverting selector css to books and added a spec to go to selector page.

--- spec/features/book_spec.rb
+++ spec/features/book_spec.rb
@@ -2,8 +2,12 @@ require 'rails_helper'
 
 describe 'Book page' do
   it 'should have content about shakespear' do
-    visit '/book_selection'
+    visit '/selector'
 
-    expect(page).to have_content('shakespeare')
+    expect(page).to have_content 'Selected Shakespeare Plays'
+    #TODO: Find a specific css property to differentiate with my selector css.
+
+
+
   end
 end
