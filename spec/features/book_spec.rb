8831ef202d566d8076d19929406f299f89227da9
Anand Alaghu <anand@loremore.com>
2015-11-02 05:56:11 -0800

    trying to remove socket: to see if travis will work.

--- spec/features/book_spec.rb
+++ spec/features/book_spec.rb
@@ -16,3 +16,6 @@ describe 'Book selector page' do
     # TODO: Find a specific css property to differentiate with my selector css.
   end
 end
+
+
+# /home/travis/build/alaghu/learn_jquery/vendor/bundle/ruby/2.2.0/gems/mysql2-0.3.18/lib/mysql2/client.rb:70:in `connect': Can't connect to local MySQL server through socket '/tmp/mysql.sock' (2) (Mysql2::Error)
