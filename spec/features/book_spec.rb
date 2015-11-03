c42b43f31807fca40788425b249bab78f70d892b
Anand Alaghu <anand@loremore.com>
2015-11-03 04:33:32 -0800

    added a case to click on the link itself.

--- spec/features/book_spec.rb
+++ spec/features/book_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'Book page' do
+describe 'Book page, which does not exist' do
   it 'should direct to selector for now since there is no index page' do
     visit '/book'
 
@@ -15,5 +15,11 @@ describe 'Book selector page' do
     expect(page).to have_content 'Selected Shakespeare Plays'
     # TODO: Find a specific css property to differentiate with my selector css.
   end
-end
 
+  it 'should accessible from home page' do
+    visit '/'
+    click_on 'A selection of books!'
+
+    expect(page).to have_content 'Selected Shakespeare Plays'
+  end
+end
