6c6e82c325812fc3ec4732a871dcc3c03370bce3
Anand Alaghu <anand@loremore.com>
2015-11-14 12:28:10 -0800

    moved the require from spec_helper to rails helper.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,11 @@ abort(message) if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
 require 'capybara/rails'
+# Need it for webkit driver
+require 'capybara/rspec'
+require 'capybara/webkit/matchers'
+Capybara.javascript_driver = :webkit
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
