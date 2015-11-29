54fac752e3dab340de849e1c741eba0d324281e2
Anand Alaghu <anand@loremore.com>
2015-11-28 20:24:07 -0800

    Revert "Revert "learn_jquery-10 : issue#10 Though I did not create a helper method, introduced erb to the link tags.""
    This reverts commit a7a24a466da6ab67a0caee8dbe261bc628009c7c.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 # Defining helper methods for calls from views/layouts/application.html
 module ApplicationHelper
   # For assets names spacing, concatenating controller and action
-  def concatenate_controller_action(controller_arg, action_arg)
+  def concatenate_controller_action(controller_arg= controller_name, action_arg= action_name)
     controller_arg + '_' + action_arg
   end
 end
